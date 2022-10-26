57d483c7e6bc894bb0f7963e8492739a9a1b4e75
Hongbusi <coderhbs@gmail.com>
2022-10-26 22:42:21 +0800

    chore: update title

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="flex flex-col h-full">
+  <div class="relative flex flex-col h-full">
+    <h1 class="absolute top-25 right-0 left-0 text-4xl text-center text-rose-400">
+      Create beautiful images of your code.
+    </h1>
     <div class="flex-1 flex justify-center items-center">
       <div class="p-4 mx-auto w-full max-w-4xl bg-gradient-to-r from-violet-500 to-fuchsia-500">
         <div class="relative p-4 bg-black/75 rounded-3">
           <div>
             <div class="flex space-x-2">
+              <div class="w-3 h-3 bg-red rounded-full" />
               <div class="w-3 h-3 bg-white/20 rounded-full" />
-              <div class="w-3 h-3 bg-white/20 rounded-full" />
-              <div class="w-3 h-3 bg-white/20 rounded-full" />
+              <div class="w-3 h-3 bg-green rounded-full" />
             </div>
           </div>
 
@@ -17,8 +20,14 @@
         </div>
       </div>
     </div>
-    <div class="min-h-40">
-      setting
+    <div class="fixed bottom-30 left-50% -translate-50% p-4 border border-white/20 rounded-2 bg-#191919 shadow">
+      <div>
+        Setting
+      </div>
+    </div>
+
+    <div class="pt-20 pb-6 text-center opacity-60">
+      Copyright © 2022-present Hongbusi
     </div>
   </div>
 </template>
