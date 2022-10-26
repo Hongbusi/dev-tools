7cb47947a6427cf3139ddf42fff0cb4cad0a0aee
Hongbusi <coderhbs@gmail.com>
2022-10-26 16:05:32 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: 'Tools',
+  meta: [
+    { name: 'description', content: 'A template to quickly create a vue project.' }
+  ]
+})
+</script>
+
+<template>
+  <router-view />
+</template>
